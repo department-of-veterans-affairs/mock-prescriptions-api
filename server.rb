841db9194752bca37c1e161725c8592f640016c3
Kam Karshenas <kam@saneshark.com>
2016-06-20 22:54:05 -0400

    changing server.rb to more closely resemble mhv sandbox

--- server.rb
+++ server.rb
@@ -1,14 +1,26 @@
 require 'webrick'
+require 'webrick/https'
 require 'json'
 
 # default port to 3000 or overwrite with PORT variable by running
 # $ PORT=3001 ruby server.rb
-port = ENV['PORT'] ? ENV['PORT'].to_i : 3004
+port = ENV['PORT'] ? ENV['PORT'].to_i : 443
 
-puts "Server started: http://localhost:#{port}/"
+cert_name = [
+  %w(CN localhost),
+]
+
+puts "Server started: https://localhost:#{port}/"
 
 root = File.expand_path './public'
-server = WEBrick::HTTPServer.new Port: port, DocumentRoot: root
+
+server = WEBrick::HTTPServer.new( Port: port,
+                                  DocumentRoot: root,
+                                  SSLEnable: true,
+                                  SSLCertName: cert_name,
+                                  SSLPrivateKey: OpenSSL::PKey::RSA.new(File.open(ENV['SSL_KEY_PATH']).read),
+                                  SSLCertificate: OpenSSL::X509::Certificate.new(File.open(ENV['SSL_CERT_PATH']).read),
+                                  SSLVerifyClient: OpenSSL::SSL::VERIFY_NONE)
 
 server.mount_proc '/' do |req, res|
   comments = JSON.parse(File.read('./json_responses/default.json', encoding: 'UTF-8'))
@@ -20,7 +32,22 @@ server.mount_proc '/' do |req, res|
   res.body = JSON.generate(comments)
 end
 
-server.mount_proc '/rx/v1/history' do |req, res|
+server.mount_proc '/mhv-api/patient/v1/session' do |req, res|
+  res['Date'] = 'Tue, 10 May 2016 16:30:17 GMT'
+  res['Server'] = 'Apache/2.2.15 (Red Hat)'
+  res['Content-Length'] = 0
+  res['Expires'] = 'Tue, 10 May 2016 16:40:17 GMT'
+  res['Token'] = 'GkuX2OZ4dCE=48xrH6ObGXZ45ZAg70LBahi7CjswZe8SZGKMUVFIU88='
+  res['Set-Cookie'] = 'JSESSIONID=QWMKXyMZmBfnC15xJKTQRMPvpMpsRhPJNwc9PGQMhSR8QQ8gQLL8!1523265843; path=/; HttpOnly'
+  res['X-Powered-By'] = 'Servlet/2.5 JSP/2.1'
+  res['Connection'] = 'close'
+  res['Content-Type'] = 'text/plain; charset=UTF-8'
+  res['Cache-Control'] = 'no-cache'
+  res['Access-Control-Allow-Origin'] = '*'
+  res.body = nil
+end
+
+server.mount_proc '/mhv-api/patient/v1/prescription/gethistoryrx' do |req, res|
   comments = JSON.parse(File.read('./json_responses/history.json', encoding: 'UTF-8'))
 
   # always return json
@@ -30,7 +57,7 @@ server.mount_proc '/rx/v1/history' do |req, res|
   res.body = JSON.generate(comments)
 end
 
-server.mount_proc '/rx/v1/prescription' do |req, res|
+server.mount_proc '/mhv-api/patient/v1/prescription/getactiverx' do |req, res|
   comments = JSON.parse(File.read('./json_responses/prescription.json', encoding: 'UTF-8'))
 
   # always return json
@@ -40,7 +67,7 @@ server.mount_proc '/rx/v1/prescription' do |req, res|
   res.body = JSON.generate(comments)
 end
 
-server.mount_proc '/rx/v1/prescription/1435525' do |req, res|
+server.mount_proc '/mhv-api/patient/v1/prescription/1435525' do |req, res|
   comments = JSON.parse(File.read('./json_responses/prescription1435525.json', encoding: 'UTF-8'))
 
   # always return json
@@ -50,7 +77,7 @@ server.mount_proc '/rx/v1/prescription/1435525' do |req, res|
   res.body = JSON.generate(comments)
 end
 
-server.mount_proc '/rx/v1/tracking/1435525' do |req, res|
+server.mount_proc '/mhv-api/patient/v1/prescription/rxtracking/1435525' do |req, res|
   comments = JSON.parse(File.read('./json_responses/tracking1435525.json', encoding: 'UTF-8'))
 
   # always return json
