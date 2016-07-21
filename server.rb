7e892316d208319d49361da5fa0b41c2477bc0ed
cfeeney-va <christine.feeney.va@gmail.com>
2016-07-20 23:14:35 -0400

    Add GET responses for MHV SM API (#4)

--- server.rb
+++ server.rb
@@ -45,6 +45,18 @@ server.mount_proc '/rx/v1/session' do |req, res|
   res.body = nil
 end
 
+server.mount_proc '/mhv-sm-api/patient/v1/session' do |req, res|
+  res['Date'] = 'Tue, 10 May 2016 16:30:17 GMT'
+  res['Server'] = 'Apache/2.2.15 (Red Hat)'
+  res['Content-Length'] = 0
+  res['Expires'] = 'Tue, 10 May 2017 16:40:17 GMT'
+  res['Token'] = 'GkuX2OZ4dCE=48xrH6ObGXZ45ZAg70LBahi7CjswZe8SZGKMUVFIU88='
+  res['X-Powered-By'] = 'Servlet/2.5 JSP/2.1'
+  res['Connection'] = 'close'
+  res['Content-Type'] = 'text/plain; charset=UTF-8'
+  res.body = nil
+end
+
 server.mount_proc '/mhv-api/patient/v1/prescription/gethistoryrx' do |req, res|
   generate_response(res, './json_responses/history.json')
 end
@@ -75,6 +87,34 @@ end
 
 server.mount_proc '/rx/v1/tracking/1435525' do |req, res|
   generate_response(res, './json_responses/tracking1435525.json')
+end
+
+server.mount_proc '/mhv-sm-api/patient/v1/triageteam' do |req, res|
+  generate_response(res, './json_responses/triageteam.json')
+end
+
+server.mount_proc '/mhv-sm-api/patient/v1/folder' do |req, res|
+  generate_response(res, './json_responses/folder.json')
+end
+
+server.mount_proc '/mhv-sm-api/patient/v1/folder/-1' do |req, res|
+  generate_response(res, './json_responses/folder-1.json')
+end
+
+server.mount_proc '/mhv-sm-api/patient/v1/folder/-1/message/page/1/pageSize/10' do |req, res|
+  generate_response(res, './json_responses/folder-1message.json')
+end
+
+server.mount_proc '/mhv-sm-api/patient/v1/message/573059/read' do |req, res|
+  generate_response(res, './json_responses/message573059read.json')
+end
+
+server.mount_proc '/mhv-sm-api/patient/v1/message/573059/history' do |req, res|
+  generate_response(res, './json_responses/message573059history.json')
+end
+
+server.mount_proc '/mhv-sm-api/patient/v1/message/category' do |req, res|
+  generate_response(res, './json_responses/messagecategory.json')
 end
 
 def generate_response(res, file_location)
